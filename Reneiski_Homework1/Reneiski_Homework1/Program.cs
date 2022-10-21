4e7184ff323387f23a4c1edac6bd34c012e68f7d
Aliaksandr Reneiski <agrren@tut.by>
2022-10-21 14:43:19 +0300

    Homework commit

---
+++ Reneiski_Homework1/Reneiski_Homework1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hi! What's your name?");
+string name = Console.ReadLine();
+Console.WriteLine("Nice to meet you, " + name + "!");
+Console.ReadLine();

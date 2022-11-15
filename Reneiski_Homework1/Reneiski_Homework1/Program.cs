f15b0fa9dcb83dca662024f6d51ea8b1062ebb44
Aliaksandr Reneiski <agrren@tut.by>
2022-11-15 09:43:50 +0200

    Lecture 5

--- Reneiski_Homework1/Reneiski_Homework1/Program.cs
+++ Reneiski_Homework1/Reneiski_Homework1/Program.cs
@@ -1,7 +1,22 @@
-﻿class Program
+﻿using Reneiski_Homework1.Lecture4;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using Zavod;
+
+class Program
 {
     static void Main(string[] args)
     {
-        
+
+        Worker work = new Worker();
+
+        //work.GetWorker();
+        //work.NumberOfWorkers();
+        //work.DirectorWork();
+        //work.NewEmployee();
+        //work.GetWorker();
+        //work.NumberOfWorkers();
+        //work.workOfMechanic();
+        work.WorkerByPosition();
     } 
 }

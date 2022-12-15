c20b52ea9602fefc020110196f5452dfb3ac94d5
Aliaksandr Reneiski <agrren@tut.by>
2022-12-15 17:17:21 +0200

    Lecture8homework

---
+++ Reneiski_Homework1/Reneiski_Homework1/Lecture8/ShowMassiveElement.cs
@@ -0,0 +1,51 @@
+﻿namespace Reneiski_Homework1.Lecture8
+{
+    public class ShowMassiveElement
+    {        
+
+        public void ShowMassive()
+        {
+            try
+            {
+                int[] massive = { 8, 7, 1, 4, 2 };
+
+                Console.WriteLine("Input index of element in massive:");
+
+                string? inputedValue = Console.ReadLine();
+
+                string? checkedValue = inputedValue.Equals(string.Empty) ? null : inputedValue;
+
+                int inputtedNumber = Int32.Parse(checkedValue);
+
+                if (inputtedNumber == 0)
+                {
+                    throw new ZeroIndexException();
+                }
+
+                int massiveElement = massive[inputtedNumber];
+
+                Console.WriteLine($"Massive element that has index {inputedValue} has value {massiveElement}");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Exception is worrked off");
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Exception is worrked off");
+                Console.WriteLine(ex.Message);
+            }
+            catch (System.FormatException ex)
+            {
+                Console.WriteLine("Exception is worrked off");
+                Console.WriteLine(ex.Message);
+            }
+            catch (ZeroIndexException ex)
+            {
+                Console.WriteLine("Exception is worrked off");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
--- Reneiski_Homework1/Reneiski_Homework1/Program.cs
+++ Reneiski_Homework1/Reneiski_Homework1/Program.cs
@@ -1,28 +1,14 @@
-using Reneiski_Homework1.Lecture5;
-using WorkerNamespace;
+using Reneiski_Homework1.Lecture8;
 
 namespace Reneiski_Homework1
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Zavod Belaz = new Zavod(new Worker(), "Belaz", "The best of the best!");
-            Worker sasha = new Worker("Alex", "Ren", 41, "QA");
-            Worker ivan = new Worker("Ivan", "Ivanov", 54, "Director");
-            Worker semen = new Worker("Semen", "Semenov", 51, "QA");
+            ShowMassiveElement showMassiveElement = new ShowMassiveElement();
 
-            Belaz.SetWorker(sasha);
-            Belaz.SetWorker(ivan);
-            Belaz.SetWorker(semen);
-
-            Belaz.ShowAllEmployeeInfo();
-            Belaz.ShowEmployeeNameInfo();
-            Belaz.ShowEmployeeAgeInfo();
-            Belaz.ShowEmployeePositionInfo();
-            Belaz.ShowEmployeeNumber();
-            Belaz.ShowEmployeeByPosition();
-            Belaz.ShowEmployeeWork();
+            showMassiveElement.ShowMassive();
         }
     }
 }
